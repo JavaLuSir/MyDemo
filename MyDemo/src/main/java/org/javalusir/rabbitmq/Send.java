cfeaaf165bb760b0722f40ed92c41262749de139
lu <javalusir@163.com>
2016-11-08 10:17:29 +0800

    rabbitmq demo

---
+++ MyDemo/src/main/java/org/javalusir/rabbitmq/Send.java
@@ -0,0 +1,25 @@
+package org.javalusir.rabbitmq;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+public class Send {
+
+	public static void main(String[] args) throws IOException, TimeoutException{
+		ConnectionFactory connectFactory = new ConnectionFactory();
+		connectFactory.setHost("localhost");
+		Connection connection = connectFactory.newConnection();
+		Channel channel = connection.createChannel();
+		channel.queueDeclare("hello", false, false, false, null);
+		String message = "hello world!";
+		channel.basicPublish("", "hello", null, message.getBytes());
+		System.out.println(" [x] Sent '" + message + "'");
+		
+		channel.close();
+		connection.close();
+	}
+}
