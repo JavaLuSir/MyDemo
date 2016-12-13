7579defdfd371df00b73a3cad89e8a42c9775902
lu <javalusir@163.com>
2016-12-14 06:30:57 +0800

    add robot demo

---
+++ MyDemo/src/main/java/org/javalusir/robot/MyRobot.java
@@ -0,0 +1,20 @@
+package org.javalusir.robot;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+/**
+ * a robot demo it can auto do something
+ * @author lu
+ *
+ */
+public class MyRobot {
+
+	public static void main(String[] args) throws AWTException, InterruptedException{
+		Robot robot = new Robot();
+		
+		robot.mouseMove(500, 600);
+		robot.mousePress(InputEvent.BUTTON1_MASK);
+		robot.mouseRelease(InputEvent.BUTTON1_MASK);
+	}
+}
